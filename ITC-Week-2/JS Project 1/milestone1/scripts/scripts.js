95c24d4d0efaf8743ae9d88f51a74736cf59a1f0
Dahvid NessAiver <dahvid@Dahvids-MacBook-Pro-2.local>
2022-10-30 13:05:33 +0200

    Created base files for all milestones.

---
+++ ITC-Week-2/JS Project 1/milestone1/scripts/scripts.js
@@ -0,0 +1,5 @@
+let xValue = 4;
+let yValue = 8;
+
+document.getElementById("x").innerText = xValue;
+document.getElementById("y").innerText = yValue;
---
+++ ITC-Week-2/JS Project 1/milestone5/scripts/scripts.js
@@ -0,0 +1,16 @@
+let xValue = 3;
+let yValue = 5;
+
+document.getElementById("x").innerText = xValue;
+document.getElementById("y").innerText = yValue;
+
+// function fibonacci() {
+//     let x = xValue;
+//     console.log(x);
+//     let y = yValue;
+//     console.log(y)
+//     let z = x + y;
+//     console.log(z);
+//     document.getElementById('x').innerText = x;
+//     document.getElementById('y').innerText = y;
+// }
